ea41a7a48354c929ea9602b8591cfd2e534589c0
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-27 20:31:59 -0400

    Exercise_3

--- Exercise_3_List_Less_Than_Ten.py
+++ Exercise_3_List_Less_Than_Ten.py
@@ -6,3 +6,10 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # and write a program that prints out all the elements of the list that are less than 5.
 
 # -------
+
+less_than_five = []
+for number in a:
+    if number < 5:
+        less_than_five.append(number)
+
+print(less_than_five)
