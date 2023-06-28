c712cdad64291aa14dfdd80d9bb7611e8c4f68b2
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-06-28 00:27:06 -0400

    Exercise_5

--- Exercise_5_List_Overlap.py
+++ Exercise_5_List_Overlap.py
@@ -1,11 +1,23 @@
 # Exercise 5
 # Take two lists, say for example these two:
 
-#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-#   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
+# Make sure your program works on two lists of different sizes.
 
 # Extras:
 
 # Randomly generate two lists to test this
 # Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+
+# -------
+
+same_elements_list = []
+
+for number in a:
+    if number in b and number not in same_elements_list:
+        same_elements_list.append(number)
+
+print(same_elements_list)
