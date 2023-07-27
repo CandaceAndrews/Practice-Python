54d4be25dd5831470ee5423d18b4dc26240b44ae
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-27 18:56:43 -0400

    new lesson started

---
+++ Exercise_8_Rock_Paper_Scissors.py
@@ -0,0 +1,18 @@
+# Make a two-player Rock-Paper-Scissors game.
+# (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner,
+# and ask if the players want to start a new game)
+
+# Remember the rules:
+
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+
+#  -------
+
+player_choice = input("Please Choose: Paper, Rock or Scissors")
+
+def paper_rock_scissors_game(decision):
+    quit_game = 
+    while decision != "exit":
+        
