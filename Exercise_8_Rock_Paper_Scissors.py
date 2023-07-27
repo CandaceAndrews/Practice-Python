103c18a4a70496fdcc6902d987d28698b6f94fcf
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-27 19:17:41 -0400

    new lesson started

--- Exercise_8_Rock_Paper_Scissors.py
+++ Exercise_8_Rock_Paper_Scissors.py
@@ -1,3 +1,4 @@
+import random
 # Make a two-player Rock-Paper-Scissors game.
 # (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner,
 # and ask if the players want to start a new game)
@@ -10,9 +11,11 @@
 
 #  -------
 
-player_choice = input("Please Choose: Paper, Rock or Scissors")
-
 def paper_rock_scissors_game(decision):
-    quit_game = 
-    while decision != "exit":
-        
+    quit_game = "No"
+    options = ["paper", "rock", "scissors"]
+    player_choice = input("Please Choose: Paper, Rock or Scissors").lower()
+    computer_choice = random.choice(options).lower()
+    
+    while quit_game != "exit":
+        if player_choice == "paper" and 
