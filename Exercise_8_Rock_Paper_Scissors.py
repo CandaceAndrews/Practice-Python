5eef4254c5bcc38a47a3a7ae89c0b9a392152a4e
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-27 19:51:25 -0400

    new lesson started

--- Exercise_8_Rock_Paper_Scissors.py
+++ Exercise_8_Rock_Paper_Scissors.py
@@ -12,19 +12,46 @@ import random
 #  -------
 
 
-def paper_rock_scissors_game(decision):
+def paper_rock_scissors_game():
     quit_game = "No"
     options = ["paper", "rock", "scissors"]
-    player_choice = input("Please Choose: Paper, Rock or Scissors").lower()
+    player_choice = input("Please Choose: Paper, Rock or Scissors:  ").lower()
     computer_choice = random.choice(options).lower()
-    
+
     lose = "You Lose!"
     win = "You win!"
 
-    while quit_game != "exit":
+    while quit_game != "yes":
         if player_choice == computer_choice:
+            print(computer_choice)
             print("Tie!")
+            quit_game = input("Keep playing? Yes or No:  ").lower
         elif player_choice == "paper" and computer_choice == "rock":
+            print(computer_choice)
             print(win)
-        elif player_choice == "rock" and computer_choice = "paper":
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        elif player_choice == "paper" and computer_choice == "scissors":
+            print(computer_choice)
             print(lose)
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        elif player_choice == "rock" and computer_choice == "paper":
+            print(computer_choice)
+            print(lose)
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        elif player_choice == "rock" and computer_choice == "scissors":
+            print(computer_choice)
+            print(win)
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        elif player_choice == "scissors" and computer_choice == "paper":
+            print(computer_choice)
+            print(win)
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        elif player_choice == "scissors" and computer_choice == "rock":
+            print(computer_choice)
+            print(lose)
+            quit_game = input("Keep playing? Yes or No:  ").lower
+        else:
+            print("Please input valid response!")
+
+
+paper_rock_scissors_game()
