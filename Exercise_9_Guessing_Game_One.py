9335390e2be8e0d1c39212eb00bd60be982eabbe
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-28 20:30:07 -0400

    completed lesson

--- Exercise_9_Guessing_Game_One.py
+++ Exercise_9_Guessing_Game_One.py
@@ -9,3 +9,28 @@
 # Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 # -------
+import random
+
+
+def number_guessing_game():
+
+    computer_number = random.randint(1, 9)
+    player_number = 0
+    while player_number != computer_number:
+        player_number = int(input("Please select a number from 1 to 9:  "))
+
+        if player_number < computer_number:
+            print(f"{player_number} is too low!")
+        elif player_number > computer_number:
+            print(f"{player_number} is too high")
+        else:
+            print(f"Correct!!\nComputer's Number: {computer_number}")
+            break
+
+    play_again = input("Play Again: Yes or No:  ").lower()
+
+    if play_again == "yes":
+        number_guessing_game()
+
+
+number_guessing_game()
