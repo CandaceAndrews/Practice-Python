45c4b38fd5584a47fa7c81e8c541dbc1cda16b80
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-31 21:18:01 -0400

    completed lesson

--- Exercise_12_List_Ends.py
+++ Exercise_12_List_Ends.py
@@ -3,3 +3,15 @@
 # For practice, write this code inside a function.
 
 # -------
+
+def list_ends(user_input_list):
+    if len(user_input_list) < 2:
+        return "List should contain at least two elements."
+    first = user_input_list[0]
+    last = user_input_list[-1]
+    return [first, last]
+
+
+a = [5, 10, 15, 20, 25]
+result = list_ends(a)
+print(result)
