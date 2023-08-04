1ac666a155bc78bc7fe453c169d18624fde9c277
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-04 16:00:21 -0400

    started exercise 16

--- Exercise_16_Password_Generator.py
+++ Exercise_16_Password_Generator.py
@@ -3,3 +3,16 @@
 # password every time the user asks for a new password. Include your run-time code in a main method.
 
 # -------
+import random
+import string
+
+
+def password_generator():
+    short_password = []
+    medium_password = []
+    strong_password = []
+
+    alphabet_lower = [a, b, c, d, e, f, ]
+
+    user_request = input(
+        "Please choose password type:  Short, Medium or Strong:   ")
