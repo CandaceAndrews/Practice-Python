0d661bb7bf607b12709114288f597a935ca7b9db
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-06 19:34:18 -0400

    started exercise 18

--- Exercise_18_Cows_And_Bulls.py
+++ Exercise_18_Cows_And_Bulls.py
@@ -12,5 +12,22 @@ import random
 def generate_random_number():
     return str(random.randint(1000, 9999))
 
+
 def count_cows_and_bulls(secret_number, user_guess):
-    
+    cows = 0
+    bulls = 0
+
+    if _ in range(len(secret_number)):
+        if secret_number[_] == user_guess[_]:
+            cow += 1
+        elif user_guess[_] in secret_number:
+            bulls += 1
+    return cows, bulls
+
+
+def cows_and_bulls():
+    secret_number = generate_random_number()
+    attemps = 0
+
+    while True:
+        user_input = input("Guess a 4 digit number:  ")
