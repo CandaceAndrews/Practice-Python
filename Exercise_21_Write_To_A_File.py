4f23c7b34c1252b593e972da04d1d94236d78af9
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-14 17:37:04 -0400

    finished exercise 21

--- Exercise_21_Write_To_A_File.py
+++ Exercise_21_Write_To_A_File.py
@@ -16,3 +16,19 @@
 #         print(story_heading.contents[0].strip())
 
 # -------
+
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'http://www.nytimes.com'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text)
+
+filename = input("File to save to: ")
+
+with open(filename, 'w') as f:
+    for story_heading in soup.find_all(class_="story-heading"):
+        if story_heading.a:
+            print(story_heading.a.text.replace("\n", " ").strip())
+        else:
+            print(story_heading.contents[0].strip())
