6bd92dd977b0b861824dabd156e3dd0c0c448587
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-15 21:11:15 -0400

    started exercise 22

--- Exercise_22_Read_From_File.py
+++ Exercise_22_Read_From_File.py
@@ -1,4 +1,18 @@
 # Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file,
-# and print out the results to the screen. I have a .txt file for you, if you want to use it!
+# and print out the results to the screen.
 
 # -------
+
+counting_names_dict = {}
+
+with open('file_to_read.txt') as f:
+    line = f.readline()
+    while line:
+        line = line.strip()
+        if line in counting_names_dict:
+            counting_names_dict[line] += 1
+        else:
+            counting_names_dict[line] = 1
+        line = f.readline()
+
+print(counting_names_dict)
