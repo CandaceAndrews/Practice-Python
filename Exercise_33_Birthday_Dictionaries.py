a533d2e38ac6f71910dfb646c2e648e462a4de94
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-12 20:34:48 -0400

    started new lesson

---
+++ Exercise_33_Birthday_Dictionaries.py
@@ -0,0 +1,11 @@
+# For this exercise, we will keep track of when our friend’s birthdays are, and be able
+# to find that information based on their name. Create a dictionary (in your file) of names
+# and birthdays. When you run your program it should ask the user to enter a name, and return
+# the birthday of that person back to them.
+
+# -------
+
+birthday_dict = {}
+
+def find_birthday():
+    
